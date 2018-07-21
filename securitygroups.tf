c1a87cee964a1085de3b0d20de8f6e8cfdf3f7a7
shaunzyeev <sanjeev.s@imimobile.com>
2018-07-21 12:19:41 +0530

    Added 22 port restriction

--- securitygroups.tf
+++ securitygroups.tf
@@ -17,7 +17,7 @@ resource "aws_security_group" "externalAccess" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["183.82.177.137/32"]
   }
 
   egress {
